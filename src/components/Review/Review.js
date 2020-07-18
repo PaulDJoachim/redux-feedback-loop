b4cde235e473d8f7d5a21cb21592271aa814123d
Paul Joachim <PaulDJoachim@gmail.com>
2020-07-18 18:12:04 -0500

    basic review page working

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 
 class Review extends Component {
 
-  state = {
-    title: 'Review'
-  }
 
   render() {
     return (
       <>
-        <Header title={this.state.title}/>
-        <h1>Any Review you want to leave?</h1>
-        <Link to="/"><button>NEXT</button></Link>
+        <Header title='Review'/>
+        <div>
+          <p>Feeling: {this.props.reduxState.setFeedback.feeling}</p>
+          <p>Understanding: {this.props.reduxState.setFeedback.understanding}</p>
+          <p>Support: {this.props.reduxState.setFeedback.support}</p>
+          <p>Comments: {this.props.reduxState.setFeedback.comments}</p>
+        </div>
+        <Link to="/"><button>SUBMIT</button></Link>
       </>
     );
   }
 }
 
-export default Review;
+const mapStateToProps = (reduxState)=>({
+  reduxState
+})
+
+export default connect(mapStateToProps)(withRouter(Review));
